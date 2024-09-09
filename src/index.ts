bff5115c7a3224c58a2d1b0681f001dd9d916396
aditya1782000 <adityathakkar178@gmail.com>
2024-09-09 23:44:47 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import http from 'http';
+import app from './app';
+const server = http.createServer(app);
+
+const { API_PORT, DATABASE_URL } = process.env;
+const port = process.env.PORT || API_PORT;
+
+const mongoose = require('mongoose');
+
+function connectToDbAndRunServer() {
+    server.listen(port, async () => {
+        console.log(`Server is running on port ${port}`);
+               
+        await mongoose.connect(DATABASE_URL);
+        mongoose.connect.on('error', (error: Error) => console.log(error));
+    });
+}
+
+export default connectToDbAndRunServer();
